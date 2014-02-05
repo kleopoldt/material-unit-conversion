ef54c5974d40d4c09c0ed50b5eaf476f9ef31f67
Kurt Leopoldt <kleopoldt@gmail.com>
2014-02-04 22:18:10 -0800

    Last edits for 2/4

--- convert.js
+++ convert.js
@@ -14,10 +14,11 @@ $('input[name=prop]').click( function() {
 
 $('#MTR').submit( function (e) {
 	if ( $('#pren').is(':checked') ) {
-		convertPREN(parseFloat($('#Cr').val()),parseFloat($('#Mo').val()),parseFloat($('#N').val()),parseFloat($('#W').val()));
+		var answer = convertPREN(parseFloat($('#Cr').val()),parseFloat($('#Mo').val()),parseFloat($('#N').val()),parseFloat($('#W').val()));
 	} else { 
-		convert($('.startUnits').val(), $('.endUnits').val());
+		var answer = convert($('.startUnits').val(), $('.endUnits').val());
 	}
+	$('#answer').html(answer);
 	e.preventDefault();
 });
 
@@ -27,31 +28,31 @@ function selectMeasurement(type) {
 	console.log('my type = ' + type);
 	if (type == 'ysts') {
 		$('.endUnits, .startUnits').append(ystsOptions);
-		$('.unit_input').show();
+		$('.unit_input, .value_input label, #userInput').show();
 		$('.PRENs').hide();
 	} else if (type =='elon') {
 		$('.endUnits, .startUnits').append(elonOptions);
-		$('.unit_input').show();
+		$('.unit_input, .value_input label, #userInput').show();
 		$('.PRENs').hide();
 	} else if (type =='hard')  {
 		$('.endUnits, .startUnits').append(hardOptions);
-		$('.unit_input').show();
+		$('.unit_input, .value_input label, #userInput').show();
 		$('.PRENs').hide();
 	} else if (type =='charpy')  {
 		$('.endUnits, .startUnits').append(charpyOptions);
-		$('.unit_input').show();
+		$('.unit_input, .value_input label, #userInput').show();
 		$('.PRENs').hide();
 	} else if (type =='temp')  {
 		$('.endUnits, .startUnits').append(tempOptions);
-		$('.unit_input').show();
+		$('.unit_input, .value_input label, #userInput').show();
 		$('.PRENs').hide();
 	} else if (type =='dia')  {
 		$('.endUnits, .startUnits').append(diaOptions);
-		$('.unit_input').show();
+		$('.unit_input, .value_input label, #userInput').show();
 		$('.PRENs').hide();
 	} else if (type =='pren') {
 		$('.PRENs').show();
-		$('.unit_input').hide();
+		$('.unit_input, .value_input label, #userInput').hide();
 	}
 }
 
@@ -70,20 +71,18 @@ $('.endUnits, .startUnits').find('option').remove();
 function convert(start, end) {
 	var userInput = parseFloat($('#userInput').val());
 	if (start == 'inch' && end == 'mm') {
-		var answer = userInput * 25.4;
+		return userInput * 25.4;
 	} else if (start == 'mm' && end == 'inch') {
-		var answer = userInput / 25.4;
+		return userInput / 25.4;
 	} else if (start == 'degf' && end == 'degc') {
-		var answer = (userInput - 32) * 5/9; 
+		return (userInput - 32) * 5/9; 
 	} else if (start == 'degc' && end == 'degf') {
-		var answer = (userInput * 9/5) + 32;
+		return (userInput * 9/5) + 32;
 	}
-	$('#answer').html(answer);
-	console.log('start = ' + start + ', end = ' + end + ', input = ' + userInput);
 }
 
 function convertPREN(Cr,Mo,N,W) {
-	var answer = Cr + (3.3 * (Mo + (0.5 * W))) + 16 * N;
+	return Cr + (3.3 * (Mo + (0.5 * W))) + 16 * N;
 }
 
 $("#ysts").trigger("click");
